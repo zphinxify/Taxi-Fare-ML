06e0bcf9ae7c2a446ea0cbb9a9386875bd792c71
Zphinxify <benjamiiiin94@gmail.com>
2020-07-26 15:26:22 +0200

    Added basic files

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Taxi_fare_ML
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
